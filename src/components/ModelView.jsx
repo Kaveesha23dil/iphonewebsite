64042054c58cb2f003cec2fa3158bf541f5b2e48
Kaveesha Dilshan <kaveeshadilshankd23@gmail.com>
2024-05-03 11:53:00 +0530

    Iphone website

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,11 @@
+import { View } from "@react-three/drei"
+
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, 
+setRotationSize, size, item}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default ModelView
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://33a6ccc448afa1128f40915ea2b4cd64@o4507180056379392.ingest.us.sentry.io/4507180061556736",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
